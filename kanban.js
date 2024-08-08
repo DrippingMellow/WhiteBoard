347f9a6c77f65f5b92c4e8ccf4ef6b3b0f2f1d4a
DrippingMellow <smeloch04@gmail.com>
2024-08-08 08:36:59 +0200

    - columns capped at 10 - task now movable after resize - and more

--- kanban.js
+++ kanban.js
@@ -6,13 +6,83 @@ const stage = new Konva.Stage({
 });
 
 const layer = new Konva.Layer();
+const layertwo = new Konva.Layer();
 stage.add(layer);
+stage.add(layertwo);
 stage.on('')
 globalThis: var o = 0
-const columns = ['To Do', 'In Progress', 'Done', 'new', 'lol'];
-const columnWidth = stage.width() / columns.length;
+globalThis: var columns = ['To Do', 'In Progress', 'Done', 'new', 'lol'];
+var columnWidth = stage.width() / columns.length;
 
-columns.forEach((column, index) => {
+class ColCol {
+    initColumns() {
+        columns.forEach((column, index) => {
+
+            const column_id = "column" + o;
+            o = o + 1;
+            const group = new Konva.Group({
+                id: column_id,
+                name: "column",
+                x: index * columnWidth,
+                y: 0,
+                width: columnWidth,
+                height: stage.height(),
+                draggable: true,
+                
+            });
+        
+            const rect = new Konva.Rect({
+                width: columnWidth - 10,
+                height: stage.height() - 10,
+                fill: '#dddddda6',
+                stroke: '#000',
+                strokeWidth: 2,
+                cornerRadius: 10,
+            });
+        
+            const text = new Konva.Text({
+                text: column,
+                fontSize: 24,
+                fontFamily: 'Calibri',
+                fill: '#000',
+                padding: 10,
+                align: 'center',
+            });
+        
+            group.add(rect);
+            group.add(text);
+            layer.add(group);
+            text.on('click', function(){
+                text.setText(textarea.value)
+                rect.fill(color_pick.value)
+            })
+        });
+    };
+
+    createNewColumn(name) {
+        if (columns.length >= "10") {
+            danger.class = btn-danger;
+            return;
+        };
+        var oldColWidth = 0
+        oldColWidth = columnWidth
+        o = 0
+        layer.destroyChildren()
+        const newColumnIndex = columns.length;
+        columns.push(name);
+        columnWidth = stage.width() / columns.length;
+        layer.clear();
+        this.initColumns();
+        notes.resize_nodes(oldColWidth);
+
+        
+    };
+};
+
+const colcol = new ColCol();
+colcol.initColumns();
+
+/*columns.forEach((column, index) => {
 
     const column_id = "column" + o;
     o = o + 1;
@@ -54,154 +124,151 @@ columns.forEach((column, index) => {
     })
 });
 
-layer.draw();
+layer.draw();*/
+
 // Function to create a task
-function createNote(text, x, y, color="#fff") {
-    const local_state = state
-    const note_id = 'note' + i;
-    i = i + 1; 
-    const taskGroup = new Konva.Group({
-        x: x,
-        y: y,
-        draggable: true,
-        id: note_id,
-        name: "note",
-    });
 
-    const taskRect = new Konva.Rect({
-        width: columnWidth - 20,
-        height: 50,
-        fill: color,
-        stroke: '#000',
-        strokeWidth: 1,
-        cornerRadius: 5,
-    });
+class Notes {
+    createNote(text, x, y, color="#fff") {
+        const local_state = state
+        const note_id = 'note' + i;
+        i = i + 1; 
+        const taskGroup = new Konva.Group({
+            x: x,
+            y: y,
+            draggable: true,
+            id: note_id,
+            name: "note",
+        });
 
-    const taskText = new Konva.Text({
-        text: text,
-        fontSize: 18,
-        fontFamily: 'Calibri',
-        fill: '#000',
-        padding: 10,
-    });
+        const taskRect = new Konva.Rect({
+            width: columnWidth - 20,
+            height: 50,
+            fill: color,
+            stroke: '#000',
+            strokeWidth: 1,
+            cornerRadius: 5,
+            name: "rect",
+        });
 
-    taskGroup.add(taskRect);
-    taskGroup.add(taskText);
-    layer.add(taskGroup);
-    layer.draw();
+        const taskText = new Konva.Text({
+            text: text,
+            fontSize: 18,
+            fontFamily: 'Calibri',
+            fill: '#000',
+            padding: 10,
+        });
 
-    state.push(taskGroup)
-    
+        taskGroup.add(taskRect);
+        taskGroup.add(taskText);
+        layertwo.add(taskGroup);
+        layertwo.draw();
 
-    /*taskGroup.on('dblclick', function() {
-        // Find the text node
-        const textNode = taskText
+        state.push(taskGroup)
 
-        textNode.setText("tex");
+        taskGroup.on('mouseover', function () {
+            document.body.style.cursor = 'pointer';
+        });
+        taskGroup.on('mouseout', function () {
+            document.body.style.cursor = 'default';
+        });  
+        taskGroup.on('dblclick dbltap', function() {
+            const textNode = (taskText, taskRect)
+            taskRect.fill(color_pick.value);
+            lol(textNode)
+        });
+        taskGroup.on('dragend', function(){
+            const positi_on = taskGroup.getAbsolutePosition();
+            const ste_ee = taskGroup.id();
+            
+            save_state_change([ste_ee,positi_on])
+        });
+        taskText.on('click tap', () => {
+            var local_parent = taskGroup.id();
+            var textpos = taskText.getAbsolutePosition();
+            var stageBox = stage.container().getBoundingClientRect();
+            var areapos = {
+                x: stageBox.left + textpos.x,
+                y: stageBox.top + textpos.y,
+            };
+            var textarea = document.createElement('textarea');
+            document.body.appendChild(textarea);
+            textarea.value = taskText.text()
 
-        layer.draw();
+            textarea.style.position = 'absolute';
+            textarea.style.top = areapos.y + 'px';
+            textarea.style.left = areapos.x + 'px';
+            textarea.style.width = taskRect.width() + 'px';
+            textarea.style.height = taskRect.height() + 'px';
+            lol(areapos)
 
-    });*/
-    taskGroup.on('', function() {
-        const textNode = (taskText, taskRect)
-        taskRect.fill(color_pick.value);
+            textarea.focus();
+            textarea.addEventListener('keydown', function (e) {
+                // hide on enter
+                lol(e.key)
+                if (e.key === "Enter") {
+                    taskText.width(taskRect.width());
+                    taskText.wrap('word');
+                taskText.text(textarea.value);
+                
+                document.body.removeChild(textarea);
+                taskRect.height(taskText.height())
+                save_state_change([local_parent,taskText.text(),taskRect.height()])
+                }
+                if (e.key === "Escape") {
+                    document.body.removeChild(textarea)
+                }
+            });
+        });
+    }
 
-        lol(textNode)
-        
-        
-    });
-    taskGroup.on('dragend', function(){
-        const positi_on = taskGroup.getAbsolutePosition();
-        const ste_ee = taskGroup.id();
-        
-        save_state_change([ste_ee,positi_on])
-    })
-    taskText.on('dblclick dbltap', () => {
-        var local_parent = taskGroup.id();
-        var textpos = taskText.getAbsolutePosition();
-        var stageBox = stage.container().getBoundingClientRect();
-        var areapos = {
-            x: stageBox.left + textpos.x,
-            y: stageBox.top + textpos.y,
-        };
-        var textarea = document.createElement('textarea');
-        document.body.appendChild(textarea);
-        textarea.value = taskText.text()
+    resize_nodes(oldColWidth) {
+        const all_notes = layertwo.find('.note')
+        lol(all_notes)
 
-        textarea.style.position = 'absolute';
-        textarea.style.top = areapos.y + 'px';
-        textarea.style.left = areapos.x + 'px';
-        textarea.style.width = taskRect.width() + 'px';
-        textarea.style.height = taskRect.height() + 'px';
-        lol(areapos)
+        all_notes.forEach((item) => {
+            const itemx = item.getAbsolutePosition().x;
+            const itemy = item.getAbsolutePosition().y;
 
-        textarea.focus();
-        textarea.addEventListener('keydown', function (e) {
-            // hide on enter
-            lol(e.key)
-            if (e.key === "Enter") {
-                taskText.width(taskRect.width());
-                taskText.wrap('word');
-              taskText.text(textarea.value);
-              
-              document.body.removeChild(textarea);
-              taskRect.height(taskText.height())
-              save_state_change([local_parent,taskText.text(),taskRect.height()])
-            }
-            if (e.key === "Escape") {
-                document.body.removeChild(textarea)
-            }
-          });
-    })
-    
+            lol(itemx-10)
+            lol(oldColWidth)
+            const x = ((item.getAbsolutePosition().x - 10) / oldColWidth);
+            lol(x)
+            const newx = x * columnWidth + 10
+            lol("x: " + newx + "y: " + itemy)
+            item.position({x: newx, y: itemy})
+            var myitem = item.getChildren()
+            lol(myitem[0]);
+            myitem[0].attrs.width = columnWidth-20
+            
+
+        })
+        layertwo.draw()
+    }
+
 }
 
+const notes = new Notes()
+
 function addColumn() {
-    const newColumnIndex = columns.length;
-    columns.push('New Column');
-    const group = new Konva.Group({
-        x: newColumnIndex * columnWidth,
-        y: 0,
-        width: columnWidth,
-        height: stage.height(),
-        draggable: true,
-        name: "column"
-    });
+    value = textarea.value
+    colcol.createNewColumn(value)
 
-    const rect = new Konva.Rect({
-        width: columnWidth - 10,
-        height: stage.height() - 10,
-        fill: '#ddd',
-        stroke: '#000',
-        strokeWidth: 2,
-        cornerRadius: 10,
-    });
 
-    const text = new Konva.Text({
-        text: 'New Column',
-        fontSize: 24,
-        fontFamily: 'Calibri',
-        fill: '#000',
-        padding: 10,
-        align: 'center',
-    });
-
-    group.add(rect);
-    group.add(text);
-    layer.add(group);
-    layer.draw();
+    
+    
 
     
 }
 
 function addTask(value=textarea.value) {
     
-    createNote(value, 10, 400, color_pick);
+    notes.createNote(value, 10, 400, color_pick);
 }
 
 
 
 // Example tasks
-createNote('Task 1', 10, 100);
-createNote('Task 2', columnWidth + 10, 200);
-createNote('Task 3', 2 * columnWidth + 10, 300);
+notes.createNote('Task 1', 10, 100);
+notes.createNote('Task 2', columnWidth + 10, 200);
+notes.createNote('Task 3', 2 * columnWidth + 10, 300);
