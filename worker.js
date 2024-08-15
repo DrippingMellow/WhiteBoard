05d13fc3898dcedecd360304be74c7a706b91301
DrippingMellow <smeloch04@gmail.com>
2024-08-15 09:18:48 +0200

    changed start request

--- worker.js
+++ worker.js
@@ -10,46 +10,31 @@ function lol(value) {
 //const type = {"note": createNote(), "img_note": createImgNote(), "column": addColumn()}
 
 async function start() {
-	const requestURL = "kanban.json";
-	const request = new Request(requestURL);
-	lol(request)
+	const requestURL = "https://localhost:7064/api/TodoItems";
+	//const request = new Request(requestURL);
 
-	const response = await fetch(request);
-	lol(response)
-	const awnser = await response.json(response);
-	lol("return" + awnser)
+	$.ajax({
+		url: requestURL,
+		crossDomain: true,
+		dataType: "json jsonp",
+		success: function(data) {
+            lol(data)
+			return(data)
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.error('Error:', textStatus, errorThrown);
+		}
+	})
 }
 
 function saveBoardState() {
-    //const columns = document.querySelectorAll('.kanban_column');
-	const columns = stage.find(".column")
-	const notes = stage.find(".note")
-	const items = columns.concat(notes);
-	const boardData = [];
-
-    items.forEach(col => {
-		const item = col;
-		//col.forEach(ite => {ite.push(item)})
-		lol(item)
-		
-		//boardData.push({id: col.attrs.id, node_data: (item.attrs), child_data: item.children[0], child_data1: item.children[1]});
-		boardData.push({id: col.attrs.id, data: (col)})
-	});
-
-	lol(boardData)
-
-    global: jsonContent = JSON.stringify(boardData, null, 2);
-	localStorage.setItem("/kanban.json", jsonContent);
-
-    // Save jsonContent to a file or display it as needed
-    console.log(jsonContent);
-
+    const a = stage.toJson()
 };
 
 function save_state_change(value) {
-	var kanban = fileName= "kanban.json"
 	const id = value[0];
 	const pos = value[1];
+
 	JSON.stringify(id,pos)
 	lol(value , "saved in: ")
 }
